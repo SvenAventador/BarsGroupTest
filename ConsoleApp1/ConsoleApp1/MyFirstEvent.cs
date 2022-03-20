acf8647939b10a53cbfe0c86996c67c40d2c0dec
Sven Aventador <asumilkin67@gmail.com>
2022-03-20 16:06:46 +0300

    Second exercise on BarsGroup

---
+++ ConsoleApp1/ConsoleApp1/MyFirstEvent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class MyFirstEvent
+    {
+        public event EventHandler<char> ? OnKeyPressed;
+
+        public void Run()
+        {
+            char chars = Console.ReadKey().KeyChar;
+            while((char.ToLower(chars) != 'c') && (char.ToLower(chars) != 'с'))
+            {
+                OnKeyPressed?.Invoke(this, chars);
+                chars = Console.ReadKey().KeyChar;
+            }
+            Console.WriteLine("\nКонец программы :(");
+        }
+    }
+}
+
+
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstValue = new MyFirstEvent();
+            firstValue.OnKeyPressed += (Sender, ButtonWrite) => Console.WriteLine("\n" + ButtonWrite);
+            firstValue.Run();
+        }
+        public static void WriteChar(object sender, char value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
